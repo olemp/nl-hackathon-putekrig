71e5be717c81b6171e28c1e5adacbf10608af509
William Killerud <william@killerud.com>
2015-11-14 21:19:01 +0100

    Input field for playlist name

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,5 +1,5 @@
 var app = angular.module('home', []);
 app.controller('mainCtrl', function($scope) {
-	$scope.spotify_userdata = JSON.parse(sessionStorage["spotify.userdata"]);
+	$scope.spotify_userdata = JSON.parse(sessionStorage.getItem("spotify.userdata"));
 	console.log($scope.spotify_userdata);
 });

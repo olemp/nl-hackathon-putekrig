2d618237899e7c3e46df551e542a9c250834648b
olemp <olemp@puzzlepart.com>
2015-11-15 06:33:14 +0100

    Responsive

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -78,7 +78,7 @@ app.controller('mainCtrl', function($scope, Parse, Location, Debug) {
 	});
 
 	$scope.makePlaylist = function() {
-		$scope.makePlaylistClicked = true;
+		$scope.makingPlaylist = true;
 		spot.loadPlaylistWithName($scope.playlistName);
 		Location.getCurrent(function(location) {	
 			Parse.provider('Trip/').create({ 
